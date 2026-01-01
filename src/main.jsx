ffd5e608b11a373a6a2eb8f4e7dff3a59553420f
Matthew <zhangmatthew999@gmail.com>
2025-12-31 19:51:19 -0500

    Installing & setting up tailwind css pt.2

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import "tailwindcss";
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <h1>HHHH</h1>
   </StrictMode>,
 )
